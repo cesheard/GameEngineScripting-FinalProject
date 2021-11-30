85418f4f557a4757ae2e33681a7aa5c2327efba6
cesheard <cesheard@hotmail.com>
2021-11-29 21:09:55 -0500

    Fixed Coroutine (with help)
    It was a really simple fix of moving a line into the coroutine.

--- GES-FinalProject/Assets/Scripts/Pong/PongManager.cs
+++ GES-FinalProject/Assets/Scripts/Pong/PongManager.cs
@@ -12,12 +12,13 @@ public class PongManager : MonoBehaviour
         Debug.Log("Got to the coroutine");
         yield return new WaitForSeconds(2);
         Debug.Log("Finished coroutine");
+        Instantiate(ballPrefab, new Vector3(0, 0, 0), Quaternion.identity);
     }
 
     public void SpawnBall()
     {
         Debug.Log("Make new ball");
         StartCoroutine(Delay());
-        Instantiate(ballPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        //Instantiate(ballPrefab, new Vector3(0, 0, 0), Quaternion.identity);
     }
 }
